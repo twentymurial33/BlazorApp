6f6bbbe33b528fd86fb892bf936bc8ed1026ec58
Murial Anindo <murial.anindo@lonett.com>
2024-07-17 10:07:29 -0400

    added

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30848894fd9d87f2470f538eda1757620f8732b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26955505dceb8e4e08e943585d3abd05fbe81ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
